e81f05d259b61768c71bd239957a44b515e6ad34
Jose Sustaita <josesustaita70@gmail.com>
2020-09-08 16:32:15 -0400

    Binary search function complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,36 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+
+    # Check base case
+    if end >= start:
+
+        mid = (start + end) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == target:
+            return mid
+
+        # If element is smaller than mid, then it
+        # can only be present in left subarray
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid-1)
+
+        # Else the element can only be present
+        # in right subarray
+        else:
+            return binary_search(arr, target, mid+1, end)
+
+    else:
+        # Element is not present in the array
+        return -1
 
 
-# STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
-# the target regardless of whether the input array is
-# sorted in ascending order or in descending order
-# You can implement this function either recursively 
-# or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
+arr = [2, 3, 4, 10, 40]
+target = 40
 
+result = binary_search(arr, target, 0, len(arr)-1)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
